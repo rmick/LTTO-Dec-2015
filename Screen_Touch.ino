de87a516d41ddc7a00bcec982f4357a1195c097a
rmick <rmick@msn.com.au>
2015-12-09 21:35:15 +1100

    char* array working for button text recall
    and now my text is centred on buttons again, thanks to strlen(text)

---
+++ Screen_Touch.ino
@@ -0,0 +1,169 @@
+// char Get ButtonPress()
+// bool GetTouch()
+// void DrawButton (PosX, PosY, Width, Height, BoxColour(uint16_t), Text(string), TextSize, TextColour(uint16_t) )
+// void PrintButtonArray()      - for deBug only
+
+static int TouchX;
+static int TouchY;
+
+int Buttons [12] [4];
+char* ButtonPressed [12];
+
+///////////////////////////////////////////////////////////////////////////////
+char* GetButtonPress()
+{
+  if (GetTouch(0))
+  {
+    delay (200);  //TODO: fix the debounce properly.
+
+    // Match it to a button
+    byte row = 0;
+    for (row=0; row<12; row++) 
+    {
+      if (TouchX > Buttons [row] [0] && TouchX < Buttons [row] [2])
+      {
+        if (TouchY > Buttons [row] [1] && TouchY < Buttons [row] [3])
+        {
+          return ButtonPressed [row];
+        }
+      }
+    }
+  }  
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+bool GetTouch(bool Touched)
+{ 
+  Touched = False;
+  
+  TSPoint p = ts.getPoint();
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+
+  TouchX = map(p.x, MinValTouchX, MaxValTouchX, 0, tft.width() );
+  TouchY = map(p.y, MinValTouchY, MaxValTouchY, 0, tft.height());
+  
+  if (p.z >10) Touched = True;
+  return Touched;
+}
+
+///////////////
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+TSPoint getPressPosition()
+{
+ 
+   TSPoint p, q;
+   int upCount = 0;
+ 
+   // Wait for screen press
+   do
+   {
+      q = ts.getPoint();
+      delay(10);
+   } while( q.z < MINPRESSURE || q.z > MAXPRESSURE );
+ 
+   // Save initial touch point
+   p.x = q.x; p.y = q.y; p.z = q.z;
+ 
+   // Wait for finger to come up
+   do
+   {
+      q = ts.getPoint();
+      if ( q.z < MINPRESSURE || q.z > MAXPRESSURE  )
+      {
+         upCount++;
+      }
+      else
+      {
+         upCount = 0;
+         p.x = q.x; p.y = q.y; p.z = q.z;
+      }
+ 
+      delay(10);             // Try varying this delay
+ 
+   } while( upCount < 10 );  // and this count for different results.
+ 
+   TouchX = map(p.x, MinValTouchX, MaxValTouchX, 0, tft.width() );
+   TouchY = map(p.y, MinValTouchY, MaxValTouchY, 0, tft.height());
+ 
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+ 
+   return p;
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void DrawButton(uint16_t PosX, uint16_t PosY, uint16_t Width, uint16_t Height, uint16_t BoxColour,
+                char* Text, byte TextSize, uint16_t TextColour) 
+{
+  // Draw the button
+  tft.fillRoundRect(PosX, PosY, Width, Height, 10, BoxColour);
+  
+  // Set the text size & colour
+  tft.setTextColor(TextColour);
+  tft.setTextSize(TextSize);
+  
+  // Centre the text on the button
+  uint16_t CtrY = PosY+(Height/2)-(7*TextSize/2);                     // characters are 7 pixels high
+  uint16_t TextWidth = (strlen(Text)*5)+(strlen(Text)-1);           // characters are 5 pixels wide + 1 pixel space
+  Serial.println(strlen(Text));
+  uint16_t CtrX = PosX+(Width/2) - ((TextWidth*TextSize)/2);  
+  tft.setCursor(CtrX, CtrY);
+  tft.println(Text);
+
+  //  Create an Array that contains a list of all the buttons and their locations.
+  if (ButtonCount == 0)          //Clear the array
+  {
+    byte Row;
+    byte Column;
+    for (Row=0; Row<12; Row++)
+    {
+      for (Column=0; Column<4; Column++)
+      {
+        Buttons [Row] [Column] = 0;
+      }
+    }
+    for (Row=0; Row<12; Row++)
+    {
+      ButtonPressed [Row] = "";
+    }
+  }
+  Buttons[ButtonCount] [0]= (PosX);
+  Buttons[ButtonCount] [1]= (PosY);
+  Buttons[ButtonCount] [2]= (PosX+Width);
+  Buttons[ButtonCount] [3]= (PosY+Height);
+  ButtonPressed [ButtonCount] = Text;
+  ButtonCount++;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+void PrintButtonArray()
+{
+  return;
+  byte Row;
+  byte Column;
+  for (Row=0; Row<12; Row++)
+  {
+    for (Column=0; Column<4; Column++)
+    {
+      Serial.print (Buttons[Row] [Column]);
+      Serial.print (F(", "));
+    }
+    Serial.println();
+  }
+  for (Column=0; Column<12; Column++)
+  {
+    Serial.print (ButtonPressed [Column]);
+    Serial.print (F(", "));
+  }
+  Serial.println();
+}
+
